0db594205b94c91aa5df407cd4a52ede73e432d8
asim16111980 <asim16111980@gmail.com>
2025-06-10 22:21:40 +0200

    Update

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,19 +1,35 @@
-import { Menu, MenuButton, MenuItems } from "@headlessui/react";
-import { X,MenuIcon } from "lucide-react";
-// import MenuToggleIcon from "./MenuToggleIcon";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
+import { X, MenuIcon } from "lucide-react";
+import { navLinks } from "@/constants/navLinks";
 const NavMenu = () => {
   return (
     <div className="w-fit relative">
-      <Menu as="div" className="h-16 flex justify-center items-center relative">
-        <MenuButton  className="px-2 py-1">
-          <MenuIcon  />
-         {/* <MenuToggleIcon  /> */}
+      <Transition>
+        <Menu
+          as="div"
+          className="h-16 flex justify-center items-center relative"
+        >
+          {({ open }) => (
+            <>
+              <MenuButton className="px-2 py-1">
+                {open ? <X className="" /> : <MenuIcon />}
+              </MenuButton>
 
-        </MenuButton>
-        <MenuItems className="w-screen absolute top-full start-0">
-          <X className="absolute top-2 end-2 shadow" />
-        </MenuItems>
-      </Menu>
+              <MenuItems className="w-screen absolute top-full start-0">
+                {navLinks.map((link) => (
+                  <MenuItem></MenuItem>
+                ))}
+              </MenuItems>
+            </>
+          )}
+        </Menu>
+      </Transition>
     </div>
   );
 };
